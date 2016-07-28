28dfd03b9ba9e33546d02c222710678d68fed02d
Satya Patel <satyadpatel@knights.ucf.edu>
2016-07-27 20:42:30 -0400

    Add files via upload

---
+++ project.c
@@ -0,0 +1,68 @@
+#include "spimcore.h"
+
+
+/* ALU */
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* instruction fetch */
+int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
+{
+
+}
+
+
+/* instruction partition */
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+{
+
+}
+
+
+
+/* instruction decode */
+int instruction_decode(unsigned op,struct_controls *controls)
+{
+
+}
+
+/* Read Register */
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+
+}
+
+
+/* Sign Extend */
+void sign_extend(unsigned offset,unsigned *extended_value)
+{
+
+}
+
+/* ALU operations */
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* Read / Write Memory */
+int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+}
+
+
+/* Write Register */
+void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+{
+
+}
+
+/* PC update */
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
+
